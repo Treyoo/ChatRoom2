32b4393e9d3ba27824e1d934879b439fd216528b
Cui <cuieo@foxmail.com>
2018-08-08 11:22:39 +0800

    Action类

---
+++ src/com/action/MessageAction.java
@@ -0,0 +1,69 @@
+package com.action;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.opensymphony.xwork2.ActionSupport;
+import com.po.Message;
+import com.po.User;
+import com.service.IMessageService;
+
+public class MessageAction extends ActionSupport {
+	Message m;
+	IMessageService messageService;
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+	List<Message> messageList;
+	User u1;
+	User u2;
+	public User getU2() {
+		return u2;
+	}
+	public void setU2(User u2) {
+		this.u2 = u2;
+	}
+	public User getU1() {
+		return u1;
+	}
+	public void setU1(User u1) {
+		this.u1 = u1;
+	}
+	public List<Message> getMessageList() {
+		return messageList;
+	}
+	public void setMessageList(List<Message> messageList) {
+		this.messageList = messageList;
+	}
+	public Message getM() {
+		return m;
+	}
+	public void setM(Message m) {
+		this.m = m;
+	}
+	public void setMessageService(IMessageService messageService) {
+		this.messageService = messageService;
+	}
+	/**
+	 * 发送消息
+	 * @return
+	 */
+	public String sendMessage(){
+		messageService.sendMessage(m);
+		return SUCCESS;
+	}
+	/**
+	 * 获取消息
+	 * @return
+	 */
+	public String queryMessage(){
+		messageList=messageService.queryMessage();
+		return SUCCESS;
+	}
+	/**
+	 * 根据用户名获取聊天消息
+	 * @return
+	 */
+	public String queryMessageByUsername(){
+		messageList=messageService.queryMessageByUsername(u1, u2);
+		return SUCCESS;
+	}
+}
---
+++ src/com/action/UserAction.java
@@ -0,0 +1,75 @@
+package com.action;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.po.User;
+import com.service.IUserService;
+
+public class UserAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+	User u;
+	IUserService userService;
+	String info;
+	List<User> userList;
+	public List<User> getUserList() {
+		return userList;
+	}
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public void setUserService(IUserService userService) {
+		this.userService = userService;
+	}
+	public User getU() {
+		return u;
+	}
+	public void setU(User u) {
+		this.u = u;
+	}
+	
+	public String register(){
+		userService.register(u);
+		return SUCCESS;
+	}
+	public String login(){
+		if(userService.login(u)){
+			
+			return SUCCESS;
+		}
+		info="用户名或密码错误！";
+		return INPUT;
+	}
+	public String logout(){
+		userService.logout(u);
+		return SUCCESS;
+	}
+	public String queryOnline(){
+		userList=userService.queryOnline();
+		return SUCCESS;
+	}
+	public String searchUserByName(){
+		userList=userService.searchUserByName(u.getUsername());
+		return SUCCESS;
+	}
+	public String getUserInfoByName(){
+		u=userService.getUserInfoByName(u.getUsername());
+		return SUCCESS;
+	}
+	/**验证用户名是否存在*/
+	public String validateUsername(){
+		if(userService.getUserInfoByName(u.getUsername())!=null){
+			info="has";
+		}else{
+			info="no";
+		}
+		return SUCCESS;
+	}
+}
